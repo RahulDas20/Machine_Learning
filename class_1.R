ca9930a717fb5b7945568d84375d859e8b73404b
Rahul Das <rahulbd6546@gmail.com>
2025-09-07 11:29:20 +0600

    new features

--- class_1.R
+++ class_1.R
@@ -69,3 +69,22 @@ best_cutoff
 read_mnist()
 
 str(mnist_27)
+
+#this line of code develops a prediction of male and female depeded on best cutoff
+#then the 
+y_hat <- ifelse(test_set$height > best_cutoff,"Male","Female") %>%
+  factor(levels = levels(test_set$sex))
+
+y_hat
+y_hat <- factor(y_hat)
+#Day 2 Class
+
+y_hat
+
+table(predicted = y_hat, actual = test_set$sex)
+
+
+test_set %>%
+  mutate(y_hat = y_hat) %>%
+  group_by(sex) %>%
+  summarise(accuracy = mean(y_hat == sex))
